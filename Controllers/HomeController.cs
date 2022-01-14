83222fdfecf50c3427d53be48eff1a908b593148
Snehal-Patil7890 <patilsnehal7890@gmail.com>
2022-01-14 18:28:43 +0530

    Changes made

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,15 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Shopping_Site.Models;
 
 namespace Shopping_Site.Controllers
 {
     public class HomeController : Controller
     {
+        ShoppingSiteDatabaseEntities2 db = new ShoppingSiteDatabaseEntities2();
         // GET: Home
-        public ActionResult Home()
+
+        [HttpGet]
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserDetail u)
+        {
+            UserDetail existinguser = db.UserDetails.Where(temp => temp.UserName == u.UserName && temp.Password==u.Password).FirstOrDefault();
+            if (existinguser==null)
+            {
+                ViewBag.message="Not an existing user";
+                return View();
+            }
+            Session["userId"] = existinguser.UserID;
+            Session["name"] = existinguser.UserName;
+            return RedirectToAction("Index", "Products");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string Name,UserDetail u)
+        {
+            ViewBag.Name = Name;
+            UserDetail existingusername = db.UserDetails.Where(temp => temp.UserName == u.UserName).FirstOrDefault();
+            if(existingusername != null)
+            {
+                ViewBag.msg1 = "Username is not valid";
+                return View();
+            }
+            db.UserDetails.Add(u);
+            db.SaveChanges();
+            return RedirectToAction("Login","Home");
         }
     }
 }
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shopping_Site.Models;
+
+namespace Shopping_Site.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ShoppingSiteDatabaseEntities2 db = new ShoppingSiteDatabaseEntities2();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<ShoppingProduct> Products = db.ShoppingProducts.ToList();
+            return View(Products);
+        }
+
+        [HttpGet]
+        public ActionResult Add(long id)
+        {
+            ShoppingProduct existingProduct = db.ShoppingProducts.Where(temp => temp.ProductID == id).FirstOrDefault();
+            return View(existingProduct);
+        }
+
+        [HttpPost]
+        public ActionResult Add(ShoppingProduct p)
+        {
+            ShoppingProduct existingProduct = db.ShoppingProducts.Where(temp => temp.ProductID == p.ProductID).FirstOrDefault();
+            Cart newcart = new Cart();
+            newcart.UserID = Convert.ToInt64(Session["userId"]);
+            newcart.ProductName = existingProduct.ProductName;
+            newcart.ProductPrice = existingProduct.ProductPrice;
+            db.Carts.Add(newcart);
+            db.SaveChanges();
+            return RedirectToAction("Mycart", "Products");
+        }
+
+        public ActionResult Remove(long id)
+        {
+            Cart cartProduct = db.Carts.Where(temp => temp.CartID == id).FirstOrDefault();
+            db.Carts.Remove(cartProduct);
+            db.SaveChanges();
+            return RedirectToAction("Mycart","Products");
+        }
+        public ActionResult Mycart()
+        {
+            long userID = Convert.ToInt64(Session["userId"]);
+            List<Cart> mycarts = db.Carts.Where(temp => temp.UserID == userID ).ToList();
+            return View(mycarts);
+        }
+        public ActionResult LogOut()
+        {
+            Session["name"] = null;
+            Session["userId"] = null;
+            return RedirectToAction("Login","Home");
+        }
+    }
+}
